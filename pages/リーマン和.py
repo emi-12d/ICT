568150dd1239eb6371aa7ea45db9c5cfd45f9231
emi <kanon20040303@gmail.com>
2026-04-22 13:54:02 +0900

    ∞のグラフ修正

--- pages/リーマン和.py
+++ pages/リーマン和.py
@@ -45,15 +45,14 @@ else:
         a = st.number_input("区域の始まり入力してください", value=0, step=1)
     with col_b:
         b = st.number_input("区域の終わりを入力してください", value=0, step=1)
-    n_val = abs(b-a) * 1000
-    c = "∞"
 
 if a > b:
     a, b = b, a
 
 # グラフ選択
-all_genre = ["右リーマン和", "左リーマン和", "中央リーマン和", "上リーマン和", "下リーマン和"]
-genre = st.multiselect("表示させたいグラフの種類を選択してください。（複数選択可）",all_genre)
+if method == '指定する':
+    all_genre = ["右リーマン和", "左リーマン和", "中央リーマン和", "上リーマン和", "下リーマン和"]
+    genre = st.multiselect("表示させたいグラフの種類を選択してください。（複数選択可）",all_genre)
 
 # 保存形式の選択
 save_format = st.radio(
@@ -61,7 +60,6 @@ save_format = st.radio(
     ["png", "svg", "jpeg"],
     horizontal=True
 )
-
 # アニメーション付きグラフの生成
 def animation_riemann(genre_type, start_n, end_n=1000):
     steps_n = np.unique(np.geomspace(start_n, end_n, num=15).astype(int))
@@ -135,49 +133,12 @@ def animation_riemann(genre_type, start_n, end_n=1000):
     return fig
 
 # 静止グラフの生成
-def plot_riemann_sum(genre_type, step_n):
-    bar_width = (b-a) / step_n # 棒の幅
-    x_split = np.linspace(a, b, step_n+1)  # n+1に均等に分ける
+def plot_riemann_sum():
     x_curve = np.linspace(a, b, 500) # 曲線用のx
     y_curve = f(x_curve) # 曲線用のy
-
-    if "右リーマン和" in genre_type:
-        x_bar = x_split[1:] - bar_width / 2 # 右端点 2番目以降を取り出す 棒の中心
-        y_bar = f(x_split[1:])
-    elif "左リーマン和" in genre_type:           
-        x_bar = x_split[:-1]  + bar_width / 2 # 右端点 2番目以降を取り出す
-        y_bar = f(x_split[:-1])
-    elif "中央リーマン和" in genre_type:
-        x_bar = x_split[:-1] + bar_width / 2
-        y_bar = f(x_bar)
-    elif "上リーマン和" in genre_type:
-        x_bar = x_split[:-1] + bar_width / 2
-        samp = 5 if step_n > 100 else 20 # 100以上なら分割数は5
-        y_bar = np.array([np.max(f(np.linspace(s, e, samp))) for s, e in zip(x_split[:-1], x_split[1:])])
-    elif "下リーマン和" in genre_type:
-        x_bar = x_split[:-1] + bar_width / 2
-        samp = 5 if step_n > 100 else 20
-        y_bar = np.array([np.min(f(np.linspace(s, e, samp))) for s, e in zip(x_split[:-1], x_split[1:])])
-
-    val = np.sum(y_bar * bar_width)
-
     fig = go.Figure() # グラフの作成
     # 関数の曲線
-    fig.add_trace(go.Scatter(x=x_curve, y=y_curve, mode='lines', name="y = f(x)"))
-    fig.add_trace(go.Bar(
-        x=x_bar, 
-        y=y_bar, 
-        width=bar_width, 
-        marker=dict(color='rgba(200, 50, 50, 0.6)')))
-    # 棒グラフ
-    fig.update_layout(
-        title=(f"{genre_type} (f(x) = {user_formula})<br>値 = {val:.5f}"),
-        xaxis_title="x",
-        yaxis_title="f(x)",
-        barmode='overlay',
-        template="plotly_white",
-        height=600
-    )
+    fig.add_trace(go.Scatter(x=x_curve, y=y_curve, fill='tozeroy', line_color = 'red', name="y = f(x)"))
     return fig
 
 def get_config(g):
@@ -201,27 +162,30 @@ def get_config(g):
         }
     }
 
+# ∞の場合のグラフの生成
+if method == '∞' and a-b != 0:
+    fig = plot_riemann_sum()
+    st.plotly_chart(fig, use_container_width=True)
+
 #区間・分割数
-if n_val!=0 and a-b!=0 and genre:
-    st.write("f(x) = " + user_formula)
-    st.write("分割数： " + str(c))
-    st.write("区間： " + str(a) + "から" + str(b))  
+if method == "指定する":
+    if n_val!=0 and a-b!=0 and genre:
+        st.write("f(x) = " + user_formula)
+        st.write("分割数： " + str(c))
+        st.write("区間： " + str(a) + "から" + str(b))  
 
-    config = {
-        'toImageButtonOptions': {
-            'format': save_format,
-            'filename': 'RiemannSum',
-            'height': None, 
-            'width': None,
-            'scale': 2
+        config = {
+            'toImageButtonOptions': {
+                'format': save_format,
+                'filename': 'RiemannSum',
+                'height': None, 
+                'width': None,
+                'scale': 2
+            }
         }
-    }
 
-    for g in genre:
-        if method == "指定する":
+        for g in genre:
             fig = animation_riemann(g, n_val)
             st.plotly_chart(fig, use_container_width=True, config=get_config(g), key=f"anim_{g}")
-        else:
-            fig = plot_riemann_sum(g, n_val)
-            st.plotly_chart(fig, use_container_width=True, config=get_config(g), key=f"anim_{g}")
+
 
