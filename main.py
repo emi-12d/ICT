9db035915642344f4f6e1ad83a322c9427c6afeb
emi <kanon20040303@gmail.com>
2026-07-08 13:59:20 +0900

    ログイン機能の追加

--- main.py
+++ main.py
@@ -1,27 +1,57 @@
 import streamlit as st
+import streamlit_authenticator as stauth
+
+import yaml
+from yaml.loader import SafeLoader
 
 st.set_page_config(page_title="ICT教材バンク")
 
-# サイドバーに表示する項目
-home = st.Page("exp/home.py", title="ホーム", default=True)
-calc_hub = st.Page("exp/mathapp.py", title="自動計算アプリ")
-image_app = st.Page("exp/imageapp.py", title="画像アプリ")
+with open('./config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
 
-# 自動計算アプリの項目
-riemann = st.Page("pages/リーマン和.py", title="リーマン和")
-regression = st.Page("pages/重回帰分析.py", title="回帰分析")
-
-# サイドバーに表示させたくない項目
-pg = st.navigation(
-    [home, calc_hub, image_app, riemann, regression],
-    position="hidden"
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days']
 )
 
-# サイドバーに表示したい項目
-with st.sidebar:
-    st.write("### メニュー")
-    st.page_link(home)
-    st.page_link(calc_hub)
-    st.page_link(image_app)
+authenticator.login()
 
-pg.run()
+# ログイン成功
+if st.session_state["authentication_status"]:
+
+    # サイドバーに表示する項目
+    home = st.Page("exp/home.py", title="ホーム", default=True)
+    calc_hub = st.Page("exp/mathapp.py", title="自動計算アプリ")
+    image_app = st.Page("exp/imageapp.py", title="画像アプリ")
+
+    # 自動計算アプリの項目
+    riemann = st.Page("pages/リーマン和.py", title="リーマン和")
+    regression = st.Page("pages/重回帰分析.py", title="回帰分析")
+
+    # サイドバーに表示させたくない項目
+    pg = st.navigation(
+        [home, calc_hub, image_app, riemann, regression],
+        position="hidden"
+    )
+
+    # サイドバーに表示したい項目
+    with st.sidebar:
+        st.write("### メニュー")
+        st.page_link(home)
+        st.page_link(calc_hub)
+        st.page_link(image_app)
+
+    pg.run()
+
+    st.sidebar.markdown("___")
+    authenticator.logout('ログアウト', "sidebar")
+
+
+elif st.session_state["authentication_status"] is False:
+    st.error('ユーザー名、またはパスワードが間違っています。')
+elif st.session_state["authentication_status"] is None:
+    st.warning('ユーザー名とパスワードを入力してください。')
+
+
